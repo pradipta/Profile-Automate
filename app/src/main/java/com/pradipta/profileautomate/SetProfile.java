e952b18c5d868788ae31bbe0accf9ccb82abbc9a
Pradipta Sarma <pradipta.sarma@UE-B-NOTE-0046.local>
2020-04-19 00:49:28 +0530

    unused imports

--- app/src/main/java/com/pradipta/profileautomate/SetProfile.java
+++ app/src/main/java/com/pradipta/profileautomate/SetProfile.java
@@ -1,29 +1,17 @@
 package com.pradipta.profileautomate;
 
-import android.annotation.SuppressLint;
 import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.os.Build;
-import android.provider.Settings;
-
-import static androidx.core.app.ActivityCompat.startActivityForResult;
-import static androidx.core.content.ContextCompat.getSystemService;
-import static androidx.core.content.ContextCompat.startActivity;
 
 public class SetProfile extends BroadcastReceiver {
     AudioManager am;
     @Override
     public void onReceive(Context context, Intent intent) {
-
-        am = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-        MediaPlayer mediaPlayer = MediaPlayer.create(context, Settings.System.DEFAULT_ALARM_ALERT_URI);
-        mediaPlayer.start();
         System.out.println("Setting to profile 0");
-        @SuppressLint("ServiceCast")
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
